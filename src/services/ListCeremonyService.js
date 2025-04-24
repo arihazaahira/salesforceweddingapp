91df37c946bb532ee169fc260a4577bdba5b2c0e
ishaq haj <ishaq.haj2@gmail.com>
2025-04-24 15:56:03 +0100

    Update the access token

--- src/services/ListCeremonyService.js
+++ src/services/ListCeremonyService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://orgfarm-c407668048-dev-ed.develop.my.salesforce.com';
-const ACCESS_TOKEN ='00DgK0000029e5F!AQEAQIWs0qUMxkBeYKlOgR0GuB2lKXkn4pDjIebwTPQEfUt4pTCMramhf1mqYiedQQaZjtkqvJK.ltXMq27ErE_mDQO51UvZ';
+const ACCESS_TOKEN ='00DgK0000029e5F!AQEAQCtKbybt1PNp5FffJ01T.ZUoq5gvtbaqVydtw1ZWvjSzMunNMwmV28OoPa.GARJT22xeoSKEMA2Y6WGKIAYhZxLP8Q4.';
 
 export const fetchAllCeremonies = async () => {
     try {
--- src/services/WeddingService.js
+++ src/services/WeddingService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ACCESS_TOKEN = '00DgK0000029e5F!AQEAQIWs0qUMxkBeYKlOgR0GuB2lKXkn4pDjIebwTPQEfUt4pTCMramhf1mqYiedQQaZjtkqvJK.ltXMq27ErE_mDQO51UvZ';
+const ACCESS_TOKEN = '00DgK0000029e5F!AQEAQCtKbybt1PNp5FffJ01T.ZUoq5gvtbaqVydtw1ZWvjSzMunNMwmV28OoPa.GARJT22xeoSKEMA2Y6WGKIAYhZxLP8Q4.';
 const INSTANCE_URL = 'https://orgfarm-c407668048-dev-ed.develop.my.salesforce.com';
 
 export const getWeddingById = async (id) => {
